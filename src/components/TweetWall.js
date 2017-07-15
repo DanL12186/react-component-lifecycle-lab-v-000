64450226d17be61feeb80ccee9d29e41f351bffb
DanL12186 <danlrcn@hotmail.com>
2017-07-15 19:45:29 -0400

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -4,15 +4,23 @@ import Tweet from './Tweet';
 class TweetWall extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       tweets: []
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  componentWillMount() { //sets stat's tweets to this.props.newTweets (getTweets from mockAPI.js)
+    this.state.tweets = this.props.newTweets
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (nextProps.newTweets > 0)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.state.tweets.unshift(nextProps.newTweets[0])
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
--- src/lib/mockAPI.js
+++ src/lib/mockAPI.js
@@ -2,14 +2,14 @@ const tweets = require('./data/tweets.json').statuses;
 let numTweetsSent = 0;
 
 export const getTweets = () => {
-  // get num tweets to send (between 0 and 4)
-  const numTweets = Math.floor(Math.random() * 4);
+  // get num tweets to send (between 1 and 4)
+  const numTweets = Math.ceil(Math.random() * 4);
 
   // if the stream has reached 100 tweets, reset
   if ((numTweetsSent + numTweets) > 100) {
     numTweetsSent = 0;
   }
-  numTweetsSent = numTweetsSent + numTweets;
+  numTweetsSent += numTweets;
 
   return tweets.slice(numTweetsSent - numTweets, numTweetsSent);
 }
